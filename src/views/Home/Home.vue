ef3c12e95deb15622488200472a7c80705a68170
Ubuntu <ubuntu@ip-172-31-12-242.ec2.internal>
2021-05-20 14:53:22 +0000

    vote/unvote en home hechos

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -13,11 +13,30 @@ export default {
         getPosts: function(){
             DataProvider("POSTS", "GET_POSTS").then((res) => {
                 let newPosts = JSON.parse(JSON.stringify(res));
-                newPosts.forEach(post => {
-                    post.voted = true;
-                    post.typePost = post.url == "" ? "ask" : "link";
+                DataProvider("POSTS", "GET_POSTS").then((result) => {
+                    let votedPosts = JSON.parse(JSON.stringify(result));
+                    newPosts.forEach(post => {
+                        let found = votedPosts.find(p => p.id == post.id);
+                        if(found != undefined){
+                            post.voted = true;
+                            console.log("si");
+                        } 
+                        else post.voted = false;
+                        post.typePost = post.url == "" ? "ask" : "link";
+                    });
+                    console.log(votedPosts);
                 });
                 this.posts = newPosts;
+            });
+        },
+        votePost: function(post){
+            DataProvider("POSTS", "VOTE_POST", post.id).then((res) => {
+              console.log(res);  
+            });
+        },
+        unvotePost: function(post){
+            DataProvider("POSTS", " UNVOTE_POST", post.id).then((res) => {
+              console.log(res);  
             });
         },
     },
