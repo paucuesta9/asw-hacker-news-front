1234eedbd70f23865ff135f3b7af3bb72fceb14d
joan.vilarrasa <joan.vilarrasa@est.fib.upc.edu>
2021-05-27 22:47:37 +0200

    added some libreary

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 // server.js
+var history = require('connect-history-api-fallback');
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.use(history())
+app.use(serveStatic(__dirname + "/dist"));
+
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/index.html'))
+})
 var port = process.env.PORT || 5000;
 app.listen(port);
+
 console.log('server started '+ port);
